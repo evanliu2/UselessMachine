020ac142863a918b7291a670537fb041b4ad5eed
Evan L <evan.x.liu@gmail.com>
2018-08-29 08:50:02 -0700

    Added countdown timer to switch to turn off after 5 seconds Switching off early cancels the timer

--- app/src/main/java/com/example/uselessmachine/MainActivity.java
+++ app/src/main/java/com/example/uselessmachine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.uselessmachine;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -11,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonSelfDestruct;
     private Switch switchUseless;
-
+    public static final String TAG = MainActivity.class.getSimpleName();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         wireWidgets();
         setListeners();
+        
     }
 
     private void wireWidgets() {
@@ -30,10 +34,11 @@ public class MainActivity extends AppCompatActivity {
         //TODO self destruct button
         switchUseless.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean checked) {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                if(checked)
+                if(isChecked)
                 {
+                    startSwitchOffTimer();
                     Toast.makeText(MainActivity.this, "On", Toast.LENGTH_SHORT).show();
                 }
 
@@ -44,6 +49,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void startSwitchOffTimer() {
+        new CountDownTimer(5000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!switchUseless.isChecked())
+                {
+                    Log.d(TAG, "onTick: Canceling...");
+                    cancel();
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+                switchUseless.setChecked(false);
+                Log.d(TAG, "onFinish: Switch set to false");
+            }
+        };
     }
 
 
